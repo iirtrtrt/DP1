5ca146797cc0db3cb5b427372df2ee0dabdffcfc
Michael Hirsch <hirschmi@student.hu-berlin.de>
2021-10-01 11:39:16 +0200

    customization of the homepage

--- src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
@@ -1,17 +1,35 @@
 package org.springframework.samples.petclinic.web;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.samples.petclinic.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
-	
-	
-	  @GetMapping({"/","/welcome"})
-	  public String welcome(Map<String, Object> model) {	    
 
-	    return "welcome";
+
+	  @GetMapping({"/","/welcome"})
+	  public String welcome(Map<String, Object> model) {
+
+          String[] names = {"Javier Váquet", "Michael Hirsch", "Kim Taekyung", "Pablo Lopez", "Alejandro Nuñez", "Florian Gamillscheg"};
+          List<Person> persons = new ArrayList<Person>();
+
+          for(String name : names) {
+              String[] fullName = name.split(" ");
+              Person person = new Person();
+              person.setFirstName(fullName[0]);
+              person.setLastName(fullName[1]);
+              persons.add(person);
+          }
+
+          model.put("persons", persons);
+          model.put("title", "Our Projectname");
+          model.put("group", "Ling-3");
+
+          return "welcome";
 	  }
 }
