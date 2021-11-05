3fb7672ddaeccfae435f0a7f96290ff2fbc6c1fb
Christina Dsubanko <christina@dsubanko.at>
2021-11-05 10:02:18 +0100

    merged with Oca Game

---
+++ src/main/java/org/springframework/samples/parchisoca/game/OcaRepository.java
@@ -0,0 +1,7 @@
+package org.springframework.samples.parchisoca.game;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OcaRepository extends  CrudRepository<Oca, Integer>{
+    
+}
