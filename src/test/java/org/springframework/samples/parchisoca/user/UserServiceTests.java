e1a7194d14b7c1c6df789bd02af9ec4447549e6a
Pablopben <pablopben@alum.us.es>
2021-11-19 20:26:22 +0100

    errors fixed

---
+++ src/test/java/org/springframework/samples/parchisoca/user/UserServiceTests.java
@@ -0,0 +1,67 @@
+package org.springframework.samples.parchisoca.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.constraints.AssertFalse;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class UserServiceTests {
+
+    @Autowired
+    protected UserService userService;
+
+
+    @Test
+    public void findUserByUsername()
+    {
+        Optional<User> optionalUser= this.userService.findUser("flogam1");
+        assertTrue(optionalUser.isPresent());
+        assertEquals(optionalUser.get().getPassword(), "lolalola");
+    }
+
+    @Test
+    public void findNotExistingUser()
+    {
+        Optional<User> optionalUser= this.userService.findUser("Idontexist");
+        assertFalse(optionalUser.isPresent());
+    }
+
+    @Test
+    public void saveUserAndThenFindbyUsername()
+    {
+       User user = new User();
+       user.setUsername("max");
+       user.setFirstname("Max");
+       user.setLastname("Mustermann");
+       user.setPassword("verysecretpassword");
+       this.userService.saveUser(user);
+
+       Optional<User> optionalUser = this.userService.findUser("max");
+       assertTrue(optionalUser.isPresent());
+       assertEquals(optionalUser.get().getUsername(), "max");
+    }
+
+    @Test
+    public void findAuthentificationShouldReturnNull()
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        assertNull(authentication);
+    }
+
+
+}
