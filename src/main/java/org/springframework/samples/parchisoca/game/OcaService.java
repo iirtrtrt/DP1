eff2e222b3da30fbb085e9a24adfccf0999b60d3
iirtrtrt <iirtrtrt@gmail.com>
2021-11-20 16:11:31 +0100

    update

--- src/main/java/org/springframework/samples/parchisoca/game/OcaService.java
+++ src/main/java/org/springframework/samples/parchisoca/game/OcaService.java
@@ -18,8 +18,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class OcaService {
 
-    @Autowired 
-	OcaRepository ocaRepo;
+    @Autowired
+    OcaRepository ocaRepo;
 
     @Autowired
     BoardFieldRepository boardFieldRepository;
@@ -28,42 +28,39 @@ public class OcaService {
 
     GameBoardRepository gameBoardRepository;
 
-    public static final String STANDARD_FILL_COLOR  = "#fef9e7" ;
+    public static final String WHITE_COLOR = "#FFFFFF";
+    public static final String YELLOW_COLOR = "#FFFF00";
+    public static final String BROWN_COLOR = "#964B00";
 
-    public static final Integer FIELD_WIDTH  = 1;
-    public static final Integer FIELD_HEIGHT  = 1;
+    public static final Integer FIELD_WIDTH = 1;
+    public static final Integer FIELD_HEIGHT = 1;
 
-    public Optional<Oca> findById(Integer id){
-		return ocaRepo.findById(id);
-	}
+    public Optional < Oca > findById(Integer id) {
+        return ocaRepo.findById(id);
+    }
 
     @Autowired
     public OcaService(OcaRepository ocaRepository,
-                      GameRepository gameRepository, GameBoardRepository gameBoardRepository, BoardFieldRepository boardRepo) {
+        GameRepository gameRepository, GameBoardRepository gameBoardRepository, BoardFieldRepository boardRepo) {
         this.ocaRepo = ocaRepository;
         this.gameRepository = gameRepository;
         this.gameBoardRepository = gameBoardRepository;
         this.boardFieldRepository = boardRepo;
     }
 
-    public void initGameBoard(Game game){
+    public void initGameBoard(Game game) {
         //Todo: should not be hard coded
         Oca gameBoard = new Oca();
         gameBoard.background = "resources/images/background_board.jpg";
-        gameBoard.height = 816;
-        gameBoard.width = 816;
+        gameBoard.height = 800;
+        gameBoard.width = 800;
 
         //Create Game fields
         System.out.println("creating gameFields");
 
-        gameBoard.fields = new ArrayList<BoardField>();
+        gameBoard.fields = new ArrayList < BoardField > ();
         this.createGameFields(gameBoard.fields);
         System.out.println("finished creating gameFields");
-
-
-
-
-
         //game.setOther_players(user_list);
         //The following code is only for testing purposes until "Join"-function exists
 
@@ -71,36 +68,30 @@ public class OcaService {
         gameBoard.setGame(game);
         game.setGameboard(gameBoard);
 
-        try
-        {
+        try {
             this.gameBoardRepository.save(gameBoard);
-        }
-        catch (Exception e )
-        {
+        } catch (Exception e) {
             System.out.println("exception: " + e.getMessage());
         }
-
-
-
-
     }
 
 
     //Calculates all the Board Field entities that are needed 
-    public void createGameFields(List<BoardField> fields){
+    public void createGameFields(List < BoardField > fields) {
         int id;
         int column;
         int row;
 
         //ids 0 to 7
-        id=0;
+        id = 0;
         row = 7;
-
-        for(column = 0; column <= 7; column++) {
-            if(id==0){
-                fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.START, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
-            }else{
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (column = 0; column <= 7; column++) {
+            if (id == 0) {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.START, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            } else if (id == 4) {
+                fields.add(new BoardField(id, YELLOW_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            } else {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
             }
             id++;
         }
@@ -108,17 +99,26 @@ public class OcaService {
         //ids 14 to 8
         column = 7;
         id = 14;
-        for(row = 0; row <= 6; row++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (row = 0; row <= 6; row++) {
+            if (id == 8 || id == 13) {
+                fields.add(new BoardField(id, YELLOW_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            } else {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            }
             id--;
         }
 
         //ids 21 to 15
-
-        id=21;
+        id = 21;
         row = 0;
-        for(column = 0; column <= 6; column++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (column = 0; column <= 6; column++) {
+            if (id == 17) {
+                fields.add(new BoardField(id, YELLOW_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+
+            } else {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            }
+
             id--;
         }
 
@@ -126,96 +126,121 @@ public class OcaService {
         //ids 22 to 27
         column = 0;
         id = 22;
-        for(row = 1; row <= 6; row++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (row = 1; row <= 6; row++) {
+            if (id == 22 || id == 26) {
+                fields.add(new BoardField(id, YELLOW_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            } else {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            }
             id++;
         }
 
         //ids 28 to 33
         row = 6;
         id = 28;
-        for(column = 1; column <= 6; column++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.HORIZONTAL, column, row, FIELD_HEIGHT, FIELD_WIDTH ));
+        for (column = 1; column <= 6; column++) {
+            if (id == 31) {
+                fields.add(new BoardField(id, YELLOW_COLOR, FieldType.HORIZONTAL, column, row, FIELD_HEIGHT, FIELD_WIDTH));
+            } else {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.HORIZONTAL, column, row, FIELD_HEIGHT, FIELD_WIDTH));
+            }
             id++;
         }
 
         //ids 38 to 34
         column = 6;
         id = 38;
-        for(row = 1; row <= 5; row++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (row = 1; row <= 5; row++) {
+            if (id == 35) {
+                fields.add(new BoardField(id, YELLOW_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            } else {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            }
             id--;
         }
 
         //ids 43 to 39
-
-        id=43;
+        id = 43;
         row = 1;
-        for(column = 1; column <= 5; column++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (column = 1; column <= 5; column++) {
+            if (id == 40) {
+                fields.add(new BoardField(id, YELLOW_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            } else {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            }
             id--;
         }
 
         //ids 44 to 47
-
-        id=44;
+        id = 44;
         column = 1;
-        for(row = 2; row <= 5; row++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (row = 2; row <= 5; row++) {
+            if (id == 44) {
+                fields.add(new BoardField(id, YELLOW_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            } else {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            }
             id++;
         }
 
         //ids 48 to 51
-
-        id=48;
+        id = 48;
         row = 5;
-        for(column = 2; column <= 5; column++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (column = 2; column <= 5; column++) {
+            if (id == 49) {
+                fields.add(new BoardField(id, YELLOW_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            } else {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            }
             id++;
         }
 
         //ids 54 to 52
-
-        id=54;
+        id = 54;
         column = 5;
-        for(row = 2; row <= 4; row++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (row = 2; row <= 4; row++) {
+            if (id == 53) {
+                fields.add(new BoardField(id, YELLOW_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            } else {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            }
             id--;
         }
 
         //ids 57 to 55
-
-        id=57;
+        id = 57;
         row = 2;
-        for(column = 2; column <= 4; column++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (column = 2; column <= 4; column++) {
+            fields.add(new BoardField(id, WHITE_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
             id--;
         }
         //ids 58 to 59
-
-        id=58;
+        id = 58;
         column = 2;
-        for(row = 3; row <= 4; row++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (row = 3; row <= 4; row++) {
+            if (id == 58) {
+                fields.add(new BoardField(id, YELLOW_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            } else {
+                fields.add(new BoardField(id, WHITE_COLOR, FieldType.VERTICAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
+            }
             id++;
         }
-        //ids 60 to 61
 
-        id=60;
+        //ids 60 to 61
+        id = 60;
         row = 4;
-        for(column = 3; column <= 4; column++) {
-            fields.add(new BoardField(id, STANDARD_FILL_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT ));
+        for (column = 3; column <= 4; column++) {
+            fields.add(new BoardField(id, WHITE_COLOR, FieldType.HORIZONTAL, column, row, FIELD_WIDTH, FIELD_HEIGHT));
             id++;
         }
 
         //id 62
-        id=62;
-        column=3;
-        row=3;
-        fields.add(new BoardField(id, STANDARD_FILL_COLOR,  FieldType.END, column, row, 2, FIELD_HEIGHT ));
-
-        
+        id = 62;
+        column = 3;
+        row = 3;
+        fields.add(new BoardField(id, BROWN_COLOR, FieldType.END, column, row, 2, FIELD_HEIGHT));
     }
+    
     @Transactional
     public void saveOca(Oca oca) throws DataAccessException {
         ocaRepo.save(oca);
--- src/main/webapp/WEB-INF/tags/oca.tag
+++ src/main/webapp/WEB-INF/tags/oca.tag
@@ -12,6 +12,38 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var image = document.getElementById('source');
-
 ctx.drawImage(image, 0, 0, ${oca.width}, ${oca.height});
+
+ctx.beginPath();
+ctx.lineWidth = "5";
+ctx.rect( 0, 0, ${oca.width}, ${oca.height});
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(${oca.width}/20 * 3.5, ${oca.height}/20 * 3.5 , 100, 0, 2 * Math.PI);
+ctx.fillStyle = '#e32908';
+ctx.fill();
+ctx.strokeStyle = '#e32908';
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(${oca.width}/20 * 16.5, ${oca.height}/20 * 3.5 , 100, 0, 2 * Math.PI);
+ctx.fillStyle = '#0890e3';
+ctx.fill();
+ctx.strokeStyle = '#0890e3';
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(${oca.width}/20 * 16.5, ${oca.height}/20 * 16.5 , 100, 0, 2 * Math.PI);
+ctx.fillStyle = '#dbe117';
+ctx.fill();
+ctx.strokeStyle = '#dbe117';
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(${oca.width}/20 * 3.5, ${oca.height}/20 * 16.5 , 100, 0, 2 * Math.PI);
+ctx.fillStyle = '#26ca0c';
+ctx.fill();
+ctx.strokeStyle = '#26ca0c';
+ctx.stroke();
 </script>
