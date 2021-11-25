1ef1ba2320ced0f8a51d74440fbb2ea6fbc12a83
Francisco Javier Vázquez Monge <javivaz2001@gmail.com>
2021-11-25 16:38:54 +0100

    first try of tests

--- src/test/java/org/springframework/samples/parchisoca/game/ParchisServiceTest.java
+++ src/test/java/org/springframework/samples/parchisoca/game/ParchisServiceTest.java
@@ -1,17 +1,49 @@
 package org.springframework.samples.parchisoca.game;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import org.junit.Test;
+import org.junit.Assert;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.parchisoca.enums.GameStatus;
+import org.springframework.samples.parchisoca.enums.GameType;
+import org.springframework.samples.parchisoca.user.User;
+import org.springframework.samples.parchisoca.user.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.samples.parchisoca.enums.TurnState;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.AssertTrue;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 
 public class ParchisServiceTest {
+    @Autowired
+    ParchisService gameService;
 
     @Autowired
     ParchisService parchisService;
     
-
+    @Autowired
+    UserService userService;
+    
+    @Autowired
+    BoardFieldService boardFieldService;
 
     //Check the Gameboard Init method
     @Test
@@ -37,7 +69,20 @@ public class ParchisServiceTest {
             assertNotNull(field.getNumber());
         }
     }
+    @Test
+    public void movePieceFromHome() throws InterruptedException{
+        Game game = new Game();
+        game.setType(GameType.Parchis);
+        game.setName("new_game");
+        this.gameService.saveParchis(game);
+        Optional<User> optionalUser = this.userService.findUser("vazquez");
 
+
+        
+        BoardField field = boardFieldService.find(56, game.getGameboard());
+        
+        Assertions.assertEquals(game.getGameboard().getFields().get(33).getNumber(), game.getCurrent_players().get(0).getGamePieces().get(0).getField());
+    }
     
 
 
