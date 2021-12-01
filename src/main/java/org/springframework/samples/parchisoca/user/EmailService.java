135f72797270189e62c03b2a6e97655119ca780a
ALEJANDRO CARRASCO <alecarnun@alum.us.es>
2021-12-01 18:24:01 +0100

    Auto-refresh of the games

--- src/main/java/org/springframework/samples/parchisoca/user/EmailService.java
+++ src/main/java/org/springframework/samples/parchisoca/user/EmailService.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.Email;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
 
 @Component
 public class EmailService
@@ -23,13 +27,50 @@ public class EmailService
     private static final String INVITATION_TEXT_END = "Come join us!";
 
 
-    public void sendEmail(String email, String sender)
+    public void sendEmail(String email, String sender, String emailNow, String userSending)
     {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject(INVITATION_SUBJECT);
-        message.setText(GREETING_TEXT +"\n\n" + INVITATION_TEXT_1 + sender + INVITATION_TEXT_2 + "\n\n" + INVITATION_TEXT_END);
-        mailSender.send(message);
+        // SimpleMailMessage message = new SimpleMailMessage();
+        // message.setTo(email);
+        // message.setSubject(INVITATION_SUBJECT);
+        // message.setText(GREETING_TEXT +"\n\n" + INVITATION_TEXT_1 + sender + INVITATION_TEXT_2 + "\n\n" + INVITATION_TEXT_END);
+        // mailSender.send(message);
+
+
+        //This doesnt work but i think it could be something similar
+        String to = email;
+        String from = emailNow;
+ 
+      String host = "localhost";
+ 
+      Properties properties = System.getProperties();
+ 
+      properties.setProperty("mail.smtp.host", host);
+ 
+      Session session = Session.getDefaultInstance(properties);
+ 
+      try{
+         
+         MimeMessage message = new MimeMessage(session);
+ 
+         
+         message.setFrom(new InternetAddress(from));
+ 
+         
+         message.addRecipient(Message.RecipientType.TO,
+                                  new InternetAddress(to));
+ 
+         
+         message.setSubject("This is the Subject Line!");
+ 
+         
+         message.setText("This is actual message");
+ 
+        
+         Transport.send(message);
+         System.out.println("Sent message successfully....");
+      }catch (MessagingException mex) {
+         mex.printStackTrace();
+      }
 
 
     }
