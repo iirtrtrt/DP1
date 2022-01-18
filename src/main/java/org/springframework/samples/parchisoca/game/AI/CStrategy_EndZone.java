948235af55d1c048600f5ef90f3bad8d95e79fb8
Christina Dsubanko <christina@dsubanko.at>
2022-01-18 21:03:21 +0100

    AI works

--- src/main/java/org/springframework/samples/parchisoca/game/AI/CStrategy_EndZone.java
+++ src/main/java/org/springframework/samples/parchisoca/game/AI/CStrategy_EndZone.java
@@ -4,20 +4,21 @@ import org.springframework.samples.parchisoca.game.BoardFieldService;
 import org.springframework.samples.parchisoca.game.Game;
 import org.springframework.samples.parchisoca.game.Option;
 import org.springframework.samples.parchisoca.game.OptionService;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
 import java.awt.*;
 
-
+@Component
 public class CStrategy_EndZone implements AIStrategy{
 
-    private static final Map < Color, Integer> color_end = Map.of(
-        Color.RED,  134,
-        Color.BLUE, 117,
-        Color.GREEN, 151,
-        Color.YELLOW, 168
+    private static final Map < Color, Integer> color_endzone = Map.of(
+        Color.RED,  34,
+        Color.BLUE, 17,
+        Color.GREEN, 51,
+        Color.YELLOW, 68
     );
 
     
@@ -26,8 +27,10 @@ public class CStrategy_EndZone implements AIStrategy{
         //implement check here
 
         for(Option option : options){
-            Integer field_number = Integer.parseInt(option.getText().substring(Option.MOVE.length() - 1));
-            if(field_number >= color_end.get(game.getCurrent_player().getTokenColor())){
+            Integer field_number = Integer.parseInt(option.getText().substring(Option.MOVE.length()));
+            //check if player is standing before endzone
+            Integer field_before_endzone = color_endzone.get(game.getCurrent_player().getTokenColor());
+            if(field_number <= field_before_endzone && field_number > field_before_endzone - game.getDice()){
                 option.setChoosen(true);
                 optionService.saveOption(option);
                 return true;
